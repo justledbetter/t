000088c1de30928c1d66547c5744c4c88d98e30c
Brian Ledbetter <brian.ledbetter@efiia.com>
2020-11-19 11:41:43 -0500

    Updated tests

--- t_test.go
+++ t_test.go
@@ -11,9 +11,13 @@ func TestT(test *testing.T) {
 		test.Fatalf("String mismatch: not loaded yet")
 	}
 
-	// Initialize the translation system into a local object named "t"
+	// Initialize the translation system into a local object named "t" (largely for compatibility purpose)
 	t := Init("/i18n")
 
+	// Set this object as the global translator.
+	t.SetGlobal()
+
+	// Confirm the basic C translation is present
 	if t.T("hello") != "hello" {
 		test.Fatalf("String mismatch: hello")
 	}
@@ -22,6 +26,7 @@ func TestT(test *testing.T) {
 		test.Fatalf("String mismatch: error35 C")
 	}
 
+	// Confirm we can switch to a Spanish translation
 	s := t.SetLocale("es")
 	if s != "es" {
 		test.Fatalf("Failed loading translation: es got %v", s)
@@ -35,21 +40,23 @@ func TestT(test *testing.T) {
 		test.Fatalf("String mismatch: hola got %v", t.T("hello"))
 	}
 
+	// Confirm we can switch to an English translation
 	s = t.SetLocale("en_UK")
 	if s != "en_UK" {
 		test.Fatalf("Failed loading translation: en_UK got %v", s)
 	}
 
 	if t.T("com.testing.error35") != "Something improper has occurred" {
-		test.Fatalf("String mismatch: error35 es")
+		test.Fatalf("String mismatch: error35 en_UK")
 	}
 
+	// Confirm we can fallback to "standard" English after failing to load Australian English
 	s = t.SetLocale("en_AU")
 	if s != "en" {
 		test.Fatalf("Failed loading translation: en got %v", s)
 	}
 
 	if t.T("com.testing.error35") != "An error has occurred" {
-		test.Fatalf("String mismatch: error35 es")
+		test.Fatalf("String mismatch: error35 en")
 	}
 }
