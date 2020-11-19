b25cfe84ee5b7354fb91b12e38164e59a9113f38
Brian Ledbetter <brian.ledbetter@efiia.com>
2020-11-19 11:07:17 -0500

    Updated the tscan utility to automatically read and re-write the ./i18n/C.json file.  This may not be ideal in all cases, will add commandline options to adjust this behavior in the future.

--- cmd/tscan/tscan.go
+++ cmd/tscan/tscan.go
@@ -11,9 +11,26 @@ import (
 )
 
 func main() {
+
 	log.SetFlags(0)
+
+	// Default package name is t.T.
 	r := regexp.MustCompile("t\\.T\\(\"([^\"]*?)\"\\)")
+
 	out := make(map[string]string)
+
+	// TODO: allow the user to override package name
+
+	// Load existing translations.
+	if f, e := os.Open("./i18n/C.json"); e == nil {
+		defer f.Close()
+		sl, _ := ioutil.ReadAll(f)
+		json.Unmarshal(sl, &out)
+	} else {
+		log.Fatal("Unable to load existing translations from ./i18n/C.json.  Please create this file before continuing.")
+	}
+
+	// Look for strings in all .go files under the current directory.
 	e := filepath.Walk("./", func(file string, info os.FileInfo, e error) error {
 		if !strings.HasSuffix(file, ".go") {
 			return nil
@@ -40,6 +57,14 @@ func main() {
 		log.Fatal(e)
 	}
 
-	m, _ := json.MarshalIndent(out, "", "  ")
-	log.Println(string(m))
+	if f, e := os.OpenFile("./i18n/C.json",os.O_WRONLY|os.O_TRUNC|os.O_CREATE,0644); e == nil {
+		defer f.Close()
+		m, _ := json.MarshalIndent(out, "", "\t")
+		f.Write(m)
+		f.Write([]byte("\n"))
+		log.Println("Translations updated in ./i18n/C.json")
+	} else {
+		m, _ := json.MarshalIndent(out, "", "\t")
+		log.Println(string(m))
+	}
 }
